b8e492fe74db7bc293a7f8a2b9677fcc75b34e9c
mrpradeep1 <112063754+mrpradeep1@users.noreply.github.com>
2024-02-10 14:53:50 +0530

    Update HelloWorld.java
    Added new feature to add two numbers a & b

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,9 @@ public class HelloWorld
 	public static void main(String args[])
 	{
 		System.out.println("Hello World! Welcome to DevOps!!!");
+		int a = 10;
+		int b = 5;
+		int c = a + b;
+		System.out.println ("Addition of a and b =" c);		
 	}
 }
